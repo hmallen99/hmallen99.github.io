a63a76739789c8107c6b744df22c8e2ad0637038
Henry <hmallen99@gmail.com>
2023-02-20 11:28:37 -0800

    Top Bar styling

--- src/TopBarContainer.tsx
+++ src/TopBarContainer.tsx
@@ -1,12 +1,36 @@
 import * as React from "react";
 import { Outlet } from "react-router-dom";
 import PlainLink from "./PlainLink";
+import './TopBarContainer.css'
 
 export default function TopBarContainer() {
     return (
         <>
-            <div>
-                <h2>TopBar</h2>
+            <div className="top-bar-container">
+                <div className="top-bar">
+                    <PlainLink to="/">
+                        <div className="top-bar-button">
+                            Home
+                        </div>
+                    </PlainLink>
+                    <PlainLink to="/pages/blog">
+                        <div className="top-bar-button">
+                            Blog
+                        </div>
+                    </PlainLink>
+                    <PlainLink to="/pages/projects">
+                        <div className="top-bar-button">
+                            Projects
+                        </div>
+                    </PlainLink>
+                    <PlainLink to="/pages/resume">
+                        <div className="top-bar-button">
+                            Resume
+                        </div>
+                    </PlainLink>
+                </div>
+
+
                 <Outlet />
             </div>
         </>
